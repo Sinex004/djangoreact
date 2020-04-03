2a47789f5db137b6c0b9675853533dbad51427d1
Sinex004 <nurassyl.omar@gmail.com>
2020-04-03 20:05:38 +0600

    Ip configure

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'prove',
         'USER': 'postgres',
-        'PASSWORD': 'sinex',
+        'PASSWORD': '123',
         'HOST': 'localhost',
         'PORT': '',
     }
