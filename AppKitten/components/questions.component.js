c945f0e7d1588f5f2e607fc1b291c99833eecb0a
Sinex004 <nurassyl.omar@gmail.com>
2020-04-12 16:28:34 +0600

    результат считает, но радиобаттон неправильно работает

--- AppKitten/components/questions.component.js
+++ AppKitten/components/questions.component.js
@@ -5,7 +5,13 @@ import { RadioGroup, Radio, ViewPager, Divider, Icon, List, ListItem, BottomNavi
 
 
 
-export const QuestionsScreen = ({route, navigation }) => {
+export class QuestionsScreen extends React.Component {
+  state={
+    questions:null,
+    header:1,
+    sIndex:0,
+    started: true
+  }
   // useEffect(() => {
   //   console.log('staaaaart');
   //   var shuffle = require('shuffle-array');
@@ -19,39 +25,48 @@ export const QuestionsScreen = ({route, navigation }) => {
   //   setQuestions(questions)
   // }, []);
 
-  const start = () =>{
-    if(started){
+  UNSAFE_componentWillMount() {
+    // const { route,navigation } = thi s.props;
+
+    if(this.state.started){
       console.log('staaaaart');
       var shuffle = require('shuffle-array');
-      const questions = route.params;  
+      const questions = this.props.route.params;  
+      // console.log(questions)
       for (let i = 0; i<questions.length;i++){
-        questions[i]['userAnswer']= React.useState(null);
+        questions[i]['userAnswer']= null;
         questions[i]['variants'] = shuffle([questions[i].answer, questions[i].var1, questions[i].var2, questions[i].var3]);
       }
       // started = false;
+      this.setState({started:false})
+      this.setState({questions:questions})
+      console.log(questions)
       console.log('eeeeeeeeeeeennnndddddd');
-      setStarted(false)
-      return(questions)
+
     }else{
-      return(questions)
+      console.log("elseeeeee")
     }
   }
-  const [questions, setQuestions]= React.useState(start());
-  const [header, setHeader] = React.useState(1);
-  const [sIndex, setsIndex] = React.useState(0);
-  const [started,setStarted] = React.useState(true)
-  const renderLeftIcon = (style) => (
+  // const [questions, setQuestions]= React.useState(start());
+  // const [header, setHeader] = React.useState(1);
+  // const [sIndex, setsIndex] = React.useState(0);
+  // const [started,setStarted] = React.useState(true)
+  renderLeftIcon = (style) => (
     <Icon {...style} name='arrow-circle-left-outline' width={30} height={30}/>
   );
 
-  const renderRightIcon = (style) => (
+  renderRightIcon = (style) => (
     <Icon {...style} name='arrow-circle-right-outline' width={30} height={30}/>
   );
   
-  const renderSubmitIcon = (style) => (
+  renderSubmitIcon = (style) => (
     <Icon {...style} name='checkmark-circle-outline' width={30} height={30}/>
   );
-  const renderPage = (ii) => {
+  AnswerChange = e =>{
+    console.log(e);
+    this.state.questions[this.state.sIndex].userAnswer=e;
+  };
+  renderPage = (ii) => {
     return (
       <Layout key={ii.id}
         level='2'
@@ -61,8 +76,8 @@ export const QuestionsScreen = ({route, navigation }) => {
         </Layout>
         <Layout level='4' style={{flex:4, alignItems:'center'}}>
           <RadioGroup
-          selectedIndex={ii.userAnswer[0]}
-          onChange={ii.userAnswer[1]}>
+          selectedIndex={ii.userAnswer}
+          onChange={this.AnswerChange}>
             <Radio style={styles.radio} text={ii.variants[0]} /> 
             <Radio style={styles.radio} text={ii.variants[1]} />
             <Radio style={styles.radio} text={ii.variants[2]} />
@@ -73,48 +88,52 @@ export const QuestionsScreen = ({route, navigation }) => {
     );
   };
   
-  const prevPage = () => {
-    setHeader(header-1);
-    setsIndex(sIndex - 1);
+  prevPage = () => {
+    this.setState({header:this.state.header-1});
+    this.setState({sIndex:this.state.sIndex - 1});
   };
-  const nextPage = () => {
-    setHeader(header+1);
-    setsIndex(sIndex + 1);
+  nextPage = () => {
+    this.setState({header:this.state.header+1});
+    this.setState({sIndex:this.state.sIndex + 1});
   };
-  const selectPage = (number) => {
-    setHeader(number+1)
-    setsIndex(number);
+  selectPage = (number) => {
+    console.log(number)
+    this.setState({header:number+1})
+    this.setState({sIndex:number});
   };
-  const submitHandle = () => {
+  submitHandle = () => {
     var result=0;
-    for (let i = 0; i<questions.length;i++){
-      if(questions[i].variants[questions[i].userAnswer[0]]==questions[i].answer){
+    for (let i = 0; i<this.state.questions.length;i++){
+      if(this.state.questions[i].variants[this.state.questions[i].userAnswer]==this.state.questions[i].answer){
         result = result + 1;
       };
     };
-    Alert.alert(result)
-    navigation.navigate('Main')
+    Alert.alert("У тебя " + result.toString() +" правильных ответов из 10")
+    this.props.navigation.navigate('Main')
   }
-  return(
-    <SafeAreaView style={{flex:1}}>
-      <Layout style={styles.head}>
-        <Button size='tiny' onPress={prevPage} appearance='ghost' icon={renderLeftIcon}></Button>
-        <Text category='h5'>Вопрос {header}/10</Text>
-        <Button size='tiny' onPress={nextPage} appearance='ghost' icon={renderRightIcon}></Button>
+  render(){
+    return(
+      <SafeAreaView style={{flex:1}}>
+        <Layout style={styles.head}>
+          <Button size='tiny' onPress={this.prevPage} appearance='ghost' icon={this.renderLeftIcon}></Button>
+          <Text category='h5'>Вопрос {this.state.header}/10</Text>
+          <Button size='tiny' onPress={this.nextPage} appearance='ghost' icon={this.renderRightIcon}></Button>
 
-      </Layout>
-      <ViewPager style={{flex:8}}
-      selectedIndex={sIndex}
-      onSelect={selectPage}>
-      
-      {questions.map((item)=> renderPage(item))}
-      </ViewPager>
-      <Layout>
-        <Button onPress={submitHandle} icon={renderSubmitIcon}  ></Button>
-      </Layout>
-    </SafeAreaView>
-  );
+        </Layout>
+        <ViewPager style={{flex:8}}
+        selectedIndex={this.state.sIndex}
+        onSelect={this.selectPage}>
+        
+        {this.state.questions.map(item=>this.renderPage(item))}
+        </ViewPager>
+        <Layout>
+          <Button onPress={this.submitHandle} icon={this.renderSubmitIcon}  ></Button>
+        </Layout>
+      </SafeAreaView>
+    );
+  }
 };
+
 var deviceHeight = Dimensions.get("window").height;
 const styles = StyleSheet.create({
     head:{
--- AppKitten/config/index.js
+++ AppKitten/config/index.js
@@ -1,7 +1,8 @@
 const port = 8080;
-const localIP = '46.101.178.130';
-// const localIP = '192.168.0.45';
-// const localIP = '192.168.43.84'; 
+const localIP = '46.101.178.130';    //server
+// const localIP = '192.168.0.45';  //cabel
+// const localIP = '192.168.0.84';    //wifi
+// const localIP = '192.168.43.84';  //phone
 
 const apiPath = 'api';
 const baseURL = `http://${localIP}:${port}/${apiPath}`;
