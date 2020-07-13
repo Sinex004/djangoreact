ca2fe5344e9520f3e8b086b0c309130c2a80d1ca
Sinex004 <nurassyl.omar@gmail.com>
2020-07-13 17:49:06 +0600

    battle with 4 rounds with index

--- AppKitten/config/index.js
+++ AppKitten/config/index.js
@@ -1,9 +1,9 @@
 const port = 8080;
-// const localIP = '46.101.178.130';    //server
+const localIP = '46.101.178.130';    //server
 // const localIP = '192.168.0.46';  //cabel
 // const localIP = '192.168.0.84';    //wifi
 // const localIP = '192.168.43.84';  //phone
-const localIP = '192.168.0.120';    //wifi
+// const localIP = '192.168.0.120';    //wifi
 
 const apiPath = 'api';
 const baseURL = `http://${localIP}:${port}/${apiPath}`;
